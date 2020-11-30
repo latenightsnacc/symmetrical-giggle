3c0e8bfaa6951f30939a527f821ebbb5907e1a8d
Deborah <54311514+latenightsnacc@users.noreply.github.com>
2020-11-30 17:53:19 -0500

    Started the basics

---
+++ .history/nextjs-blog/components/layout_20201130171757.js
@@ -0,0 +1,23 @@
+import styles from './layout.module.css'
+import Head from 'next/head'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+const name = 'Your Name'
+export const siteTitle = 'Welcome to Mood Blogs'
+
+export default function Layout({children, home})
+ {
+ return (
+    <div className={styles.container}>
+        <Head>
+            <link rel="icon" href='/favicon.ico' />
+            <meta 
+                name="description"
+                content="Welcome! Fate willed this visit. Behold, this is a blogging platform called MoodBlogs"
+            />
+
+        </Head>
+        {children}</div>
+ )
+ }
